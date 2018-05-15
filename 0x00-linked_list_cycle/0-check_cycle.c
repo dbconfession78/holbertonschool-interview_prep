ee0a9ae94489eb9b85d0d817ea5cdbf2ed67917b
Stuart <sgkur04@gmail.com>
2018-05-15 23:41:10 +0000

    solved with fast and slow pointers

--- 0x00-linked_list_cycle/0-check_cycle.c
+++ 0x00-linked_list_cycle/0-check_cycle.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+# include "hash_tables.h"
 
 /**
  * check_cycle - checks if a linked list has a cycle
  * @list: the linked list to check for cycle
  * Return: 1 if cycle exists; 0 otherwise
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *walk = list;
+	listint_t *slow;
+	listint_t *fast;
 
-	if (list == NULL)
-		return (0);
+	slow = list;
+	fast = list;
 
-	while (walk)
+	while (slow && fast && fast->next)
 	{
-		if (walk->next == list)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
 			return (1);
-		walk = walk->next;
+		}
 	}
+
 	return (0);
 }
