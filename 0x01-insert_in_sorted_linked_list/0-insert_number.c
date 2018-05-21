af646701448b01ab40b171df5b0ecadd14523ee1
Stuart <sgkur04@gmail.com>
2018-05-21 18:09:40 +0000

    README.md 0-insert_number.c lists.h: initial commit

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a listint_t node into a sll
+ * @head: pointer to pointer of first node in sll
+ * @number: the value of inserted  node's 'n' attribute
+ * Return: pointer to head
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *walk = *head;
+	listint_t *new;
+
+	if (*head == NULL || head == NULL)
+		return (*head);
+
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+	if (walk->n >= number)
+	{
+		new->next = walk;
+		*head = new;
+		return (*head);
+	}
+	while (walk && walk->next)
+	{
+		if (walk->n <= number && walk->next->n >= number)
+		{
+			new->next = walk->next;
+			break;
+		}
+		walk = walk->next;
+	}
+	walk->next = new;
+	return (*head);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,7 @@
+0x01. Insert in sorted linked list
+
+## Description
+This project is designed to challenge my ability to write the most time and space efficient solution when inserting a node into a sorted linked list
+## Author
+
+*Stuart Kuredjian* - [Github](https://github.com/dbconfession78) || [Twitter](https://twitter.com/StueyGK)
