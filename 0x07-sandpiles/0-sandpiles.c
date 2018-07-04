49ab1fac3c0bdbca855828a7e1c7fbe2fb8188ae
Stuart Kuredjian <108@holbertonschool.com>
2018-07-04 15:56:07 +0000

    main and app file

---
+++ 0x07-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ * Return: void
+ */  
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+
+}
