e227ccac71eeb9e2526eb7b75be9fa9a6df3b93a
Stuart <sgkur04@gmail.com>
2018-07-29 04:39:36 +0000

    commit sort.h and 0-heap_sort.c

---
+++ 0x09-heap_sort/0-heap_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+
+/**
+ * heap_sort - uses heap-sort algo to sort an array of ints in ascending order
+ * @array: the array to sort
+ * @size: the size of the array
+ * Return: void
+ */
+void heap_sort(int *array, size_t size)
+{
+
+
+}
