bd063a96aeffb8e6fd6141bf2202180ef70421e1
Stuart <sgkur04@gmail.com>
2018-07-29 20:48:42 +0000

    first checker attempt

--- 0x09-heap_sort/0-heap_sort.c
+++ 0x09-heap_sort/0-heap_sort.c
@@ -9,6 +9,83 @@
  */
 void heap_sort(int *array, size_t size)
 {
+	int end = size - 1;
+
+	heapify(array, size);
+	while (end > 0)
+	{
+		swap(array, end, 0, size);
+		print_array(array, size);
+		end--;
+		sift_down(array, 0, end, size);
+	}
+}
 
 
+/**
+ * swap - swaps values of two indexes in an array
+ * @a: array whose index values are swapped
+ * @i: index of the first value
+ * @j: index of the second value
+ * @size: length of array
+ * Return: void
+ */
+void swap(int *a, int i, int j, size_t size)
+{
+	int tmp = a[j];
+
+	a[j] = a[i];
+	a[i] = tmp;
+}
+
+
+/**
+ * heapify - converts unordered array into a heap-like array
+ * @a: array to heapify
+ * @size: length of array
+ * Return: void
+ */
+void heapify(int *a, size_t size)
+{
+	int start;
+
+	start = ((int) size - 2) / 2;
+	while (start >= 0)
+	{
+		sift_down(a, start, size - 1, size);
+		start--;
+	}
+}
+
+/**
+ * sift_down - reorders array while maintaing heap
+ * @a: array to sift
+ * @start: root index
+ * @end: index of last element in heap
+ * @size: length of array
+ * Return: void
+ */
+void sift_down(int *a, int start, size_t end, size_t size)
+{
+	int root, child, child_val, next_val, root_val;
+
+	root = start;
+	while ((root * 2) + 1 <= end)
+	{
+		child = (root * 2) + 1;
+		child_val = a[child];
+		next_val = a[child + 1];
+		root_val = a[root];
+
+		if (child + 1 <= end && child_val < next_val)
+			child++;
+		if (root_val < child_val)
+		{
+			swap(a, root, child, end + 1);
+			print_array(a, size);
+			root = child;
+		}
+		else
+			return;
+	}
 }
--- 0x09-heap_sort/sort.h
+++ 0x09-heap_sort/sort.h
@@ -5,5 +5,8 @@
 
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void sift_down(int *a, int start, size_t end, size_t size);
+void heapify(int *a, size_t size);
+void swap(int *a, int i, int j, size_t size);
 
 #endif
