ff7eaf2aca79f9bd42265d4e362b2a1cc7839d7f
Stuart Kuredjian <sgkur04@gmail.com>
2018-10-05 03:54:08 +0000

    put params back in signalhandler definition

--- 0x11-log_parsing/0-stats.py
+++ 0x11-log_parsing/0-stats.py
@@ -4,32 +4,28 @@ import signal
 import sys
 
 
-def main():
-    def signal_handler():
+def signal_handler(signal, frame):
+    output()
+    sys.exit(0)
+
+
+def output():
+    print("File size: {}".format(size))
+    for code in codes:
+        if _dict.get(code):
+            print("{}: {}".format(code, _dict[code]))
+
+size = 0
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+_dict = defaultdict(int)
+
+signal.signal(signal.SIGINT, signal_handler)
+i = 1
+for line in sys.stdin:
+    line = line.split()
+    status = line[7]
+    size += int(line[8])
+    _dict[status] += 1
+    if i % 10 == 0 and i > 0:
         output()
-        sys.exit(0)
-
-    def output():
-        print("File size: {}".format(size))
-        for code in codes:
-            if _dict.get(code):
-                print("{}: {}".format(code, _dict[code]))
-
-    size = 0
-    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    _dict = defaultdict(int)
-
-    signal.signal(signal.SIGINT, signal_handler)
-    i = 1
-    for line in sys.stdin:
-        line = line.split()
-        status = line[7]
-        size += int(line[8])
-        _dict[status] += 1
-        if i % 10 == 0 and i > 0:
-            output()
-        i += 1
-
-
-if __name__ == '__main__':
-    main()
+    i += 1
