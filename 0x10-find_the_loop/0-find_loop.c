60c88a7742c9ebc00c0dd460116880ddae3bddc3
stuartkuredjian <Hyrenkosa1>
2018-10-04 20:38:15 -0700

    add find the loop

---
+++ 0x10-find_the_loop/0-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+
+/**
+ * find_listint_loop - finds the first node in a looped linked list
+ * @head: pointer to the head of linked list
+ * Return: pointer to node where loop begins
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *walk = head;
+
+	while (walk)
+	{
+		if (walk->next)
+		{
+/* printf("&walk->n: %p\n", (void *)&walk->n); */
+/* printf("&walk->next->n: %p\n", (void *)&walk->next->n); */
+/* printf("%d\n", (void *)&walk->n < (void *)&walk->next->n); */
+
+			if (&walk->n < &walk->next->n)
+			{
+				walk = walk->next;
+				return (walk);
+			}
+		}
+		walk = walk->next;
+	}
+	return (NULL);
+}
---
+++ 0x10-find_the_loop/README.md
@@ -0,0 +1,15 @@
+# 0x10. Find the Loop
+
+## Description
+This application finds which node a linked list begins a loop
+
+## Compilation
+```
+gcc -Wall -pedantic -Werror -Wextra 0-find_loop.c -L. -lloop -o find_loop
+```
+
+## Usage
+`./find_loop`
+
+## Author
+* Stuart Kuredjian* - [Github](https://github.com/dbconfession78) || [Twitter](https://twitter.com/StueyGK)
