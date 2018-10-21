3bf2c81e3a5b0b56f207c57b6f327dd25365fcb5
Stuart Kuredjian <sgkur04@gmail.com>
2018-10-21 00:38:40 +0000

    revert and return new node instead of *list

--- 0x13-double_circular_linked_list/0-add_node.c
+++ 0x13-double_circular_linked_list/0-add_node.c
@@ -6,8 +6,8 @@
  * @str: string to copy into the new node
  * Return: address of new node, or NULL on failure
  */
-List *add_node_end_1(List **list, char *str)
-/* List *add_node_end(List **list, char *str) */
+/* List *add_node_end_1(List **list, char *str) */
+List *add_node_end(List **list, char *str)
 {
 	List *first, *last, *new;
 
@@ -34,20 +34,19 @@ List *add_node_end_1(List **list, char *str)
 }
 
 /**
- * add_node_end - adds a new node to the end of a double circular linked list
+ * add_node_end_2 - adds a new node to the end of a double circular linked list
  * @list: list to modify
  * @str: string to copy into the new node
  * Return: address of new node, or NULL on failure
  */
-/* List *add_node_end_2(List **list, char *str) */
-List *add_node_end(List **list, char *str)
+List *add_node_end_2(List **list, char *str)
+ /* List *add_node_end(List **list, char *str) */
 {
-	List *last, *new_node;
+	struct List *last, *new_node;
 
 	if (*list == NULL)
 	{
-		struct List *new_node = malloc(sizeof(List));
-
+		new_node = malloc(sizeof(List));
 		new_node->str = str;
 		new_node->next = new_node->prev = new_node;
 		*list = new_node;
@@ -56,7 +55,6 @@ List *add_node_end(List **list, char *str)
 
 	last = (*list)->prev;
 	new_node = malloc(sizeof(List));
-
 	new_node->str = str;
 	new_node->next = *list;
 	(*list)->prev = new_node;
@@ -72,8 +70,8 @@ List *add_node_end(List **list, char *str)
  * @str: string to copy into the new node
  * Return: address of new node, or NULL on failure
  */
-List *add_node_begin_1(List **list, char *str)
-/* List *add_node_begin(List **list, char *str) */
+/* List *add_node_begin_1(List **list, char *str) */
+List *add_node_begin(List **list, char *str)
 {
 	List *first, *last, *new;
 
@@ -98,18 +96,18 @@ List *add_node_begin_1(List **list, char *str)
 	last->next = new;
 	*list = new;
 
-	return (*list);
+	return (new);
 }
 
 /**
- * add_node_begin - adds a new node to the beginning of a double
+ * add_node_begin_2 - adds a new node to the beginning of a double
  *                  circular linked list
  * @list: list to modify
  * @str: string to copy into the new node
  * Return: address of new node, or NULL on failure
  */
-/* List *add_node_begin_2(List **list, char *str) */
-List *add_node_begin(List **list, char *str)
+List *add_node_begin_2(List **list, char *str)
+/* List *add_node_begin(List **list, char *str) */
 {
 	struct List *last, *new_node;
 
